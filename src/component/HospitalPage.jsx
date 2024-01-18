94d105d920528d29fe1c2a216fbd9d20b935cf01
ameymn <amey.managute@gmail.com>
2024-01-18 13:09:10 +0530

    f

--- src/component/HospitalPage.jsx
+++ src/component/HospitalPage.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import donorForm from './DonorForm'
 import { useState , useEffect } from 'react'
+
 const HospitalPage = () => {
-  
+  function donorForm(e){
+    e.preventDefault();
+    window.location.href ='http://localhost:3000/hospitalPage/DonorForm'
+  }
+
   return (
     <div className="hospPage">
       <div className="grp1">
         <div className="card">
             <img src="./IMG_8367.PNG" alt="" />
-            <div className="card-content">
+            <div className="card-content" onClick={(e) => donorForm(e)}>
               <h3>Donor Card</h3>
               <p>Click Over here to create Donor Cards</p>
             </div>
